50971055fc877359a10850d98dd736df7a1f53d7
verbovenko <verbovenko@adv.ru>
2014-02-28 17:16:01 +0400

    up

--- index.php
+++ index.php
@@ -2,41 +2,65 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
 
 
-
-BitrixMigration::start();
+$bm = new BitrixMigration();
+$bm->start('restore.php');
 
 
 
 class BitrixMigration 
 {
-    protected static $arResult = array();
-    protected static $arrayName = NULL;
+    protected $arResult = array();
+    protected $arrayName = NULL;
+    protected $fullFileName = '';
+    protected $file = ''; 
+    protected $dir = '';
     
-    function init()
+    protected function init($file="restore.php")
     {
         CModule::IncludeModule("iblock");
-        self::$arrayName = 'arResult';
+        $this->arrayName = 'arResult';
+        $this->dir = $_SERVER['DOCUMENT_ROOT'].'/_adv/bm/';
+        $this->fullFileName = $this->dir.$file;
+        $this->file = $file;
         
     }
     
-    public static function start()
+    public function start()
     {
-        self::init();
-        self::ibStructure();
+        $this->init();
+        $this->getIbStructure();        
+        if ($this->writeArrayToFile($this->generateFileContent())) 
+            echo 'Файл записан. <a href="'.$this->file.'">'.$this->file.'</a>'; 
+        else 
+            echo 'возникли проблемы с записью! '.$this->fullFileName;
         
     }     
     
     
-    protected static function ibStructure()
+    protected function generateFileContent()
     {
-
-        $arResult = self::getTypesArray();
-        
-        echo self::arrayToString($arResult); 
+        $arrayString = self::arrayToString( $this->arResult ,$this->arrayName,'    ');        
+        $content = "<?php \n";        
+        $content .= $arrayString;
+        $content .= "echo '<pre>';\n";
+        $content .= "print_r(\$".$this->arrayName.");\n";
+        $content .= "echo '</pre>';\n";
+        $content .= "?>";
+        return $content;        
     }
     
     
-    protected static function getTypesArray()
+     
+    
+    
+    protected function writeArrayToFile($content)
+    {
+        return file_put_contents($this->fullFileName,$content);
+        
+    }
+              
+    
+    protected function getIbStructure()
     {
         //CModule::IncludeModule("iblock");
         $db_iblock_type = CIBlockType::GetList(array(),array("id"=>"help"));
@@ -45,16 +69,17 @@ class BitrixMigration
             if($arIBType = CIBlockType::GetByIDLang($ar_iblock_type["ID"], LANG)){
                 //echo htmlspecialcharsEx($arIBType["NAME"])."<br>";                
                                 
-                $arIBType['IBLOCKS'] = self::getIBlocksArray($arIBType['IBLOCK_TYPE_ID']);
+                $arIBType['IBLOCKS'] = $this->getIBlocksArray($arIBType['IBLOCK_TYPE_ID']);
                 $arResult[] =  $arIBType; 
                 
             }   
         }
-        return $arResult;
+        
+        $this->arResult = $arResult;        
         
     }
     
-    protected static function getIBlocksArray($type)
+    protected function getIBlocksArray($type)
     {
         $arResult = array();
         $res = CIBlock::GetList(
@@ -65,14 +90,14 @@ class BitrixMigration
             
         while($ar_res = $res->Fetch()){
             
-            $ar_res['PROPS'] = self::getIBlocksProperties($ar_res['ID']);
+            $ar_res['PROPS'] = $this->getIBlocksProperties($ar_res['ID']);
             $arResult[$ar_res['CODE']] = $ar_res;  
             
         }
         return $arResult;
     }
     
-    protected static function getIBlocksProperties($iblockId)
+    protected function getIBlocksProperties($iblockId)
     {
         $arResult = array();        
         $properties = CIBlockProperty::GetList(Array(), Array("IBLOCK_ID"=>$iblockId));
@@ -86,15 +111,15 @@ class BitrixMigration
     
     
     
-    protected static function arrayToString($array,$indent='    ')
+    protected static function arrayToString($array,$arrayName, $indent='    ')
     {                      
         if ($indent=='    ')
-            $resultText = "\$".self::$arrayName." = Array (\n";                 
+            $resultText = "\$".$arrayName." = Array (\n";                 
             
         foreach ($array as $key=>$value){            
             if (is_array($value)){
                 $resultText .= $indent."\"".$key."\" => Array (\n";                 
-                $resultText .= self::arrayToString($value,$indent.'    ');
+                $resultText .= self::arrayToString($value,'',$indent.'    ');
                 $resultText .= $indent."),\n"; 
             }else {
                 $resultText.= $indent."\"".$key."\" => \"".$value."\",\n"; 
